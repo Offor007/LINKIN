f7f8db3e868fd8da1fce975c1eb66357e353119d
unknown <offorprinceo@gmail.com>
2023-02-16 03:19:30 +0100

    feat:added new portfolio

---
+++ script.js
@@ -0,0 +1,24 @@
+//data tpye
+//let, const, var
+/* 
+let instructor = 'henry'
+instructor= 'franc'
+
+let name1 = 'enemuo'
+let name2 = 'jerry'
+let age = 22 */
+
+/* let frontend = `franc ${name1}` */
+
+let firstname = 'prince'
+firstname = 'offor'
+let age = 25
+const number = '08066973158'
+
+
+sentence = `My name is ${firstname} and i was born in ${new Date().getFullYear () - age} you can call me on ${number}`
+let wee = 'she says "her name is Ada"'
+/* alert(wee) */
+
+
+
